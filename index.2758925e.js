c97b6600cd7e3f133e89c55a5a18a212bedd2cf5
Evolyten <73497925+Evolyten@users.noreply.github.com>
2022-06-13 10:16:36 +0000

    Deploying to gh-pages from @ VitaliiKablukov/IceCream-group-2@2b2d6d6863c1962f5363557a18f2d3356aca3cdc 🚀

--- index.2758925e.js
+++ index.2758925e.js
@@ -1,2 +1,2 @@
 !function(){var o={openModalBtn:document.querySelector("[data-modal-open]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]"),body:document.querySelector("body")};function e(){document.body.classList.toggle("modal-open"),o.modal.classList.toggle("is-hidden"),o.body.classList.toggle("no-scroll")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}();
-//# sourceMappingURL=index.452ff59a.js.map
+//# sourceMappingURL=index.2758925e.js.map
--- index.bce6822c.js
+++ index.bce6822c.js
@@ -1,2 +1,2 @@
 function initMap(){var n={lat:-25.344,lng:131.031},a=new google.maps.Map(document.getElementById("map"),{zoom:4,center:n});new google.maps.Marker({position:n,map:a})}window.initMap=initMap;
-//# sourceMappingURL=index.93bb65af.js.map
+//# sourceMappingURL=index.bce6822c.js.map
