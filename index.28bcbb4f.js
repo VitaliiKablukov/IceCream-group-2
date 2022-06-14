11991dcf6791a92b43cdf7ee4485e9fb01566701
Evolyten <73497925+Evolyten@users.noreply.github.com>
2022-06-14 10:23:44 +0000

    Deploying to gh-pages from @ VitaliiKablukov/IceCream-group-2@ff056cd69b299609e300ef13507bd689585a207b 🚀

---
+++ index.28bcbb4f.js
@@ -0,0 +1,2 @@
+$(document).ready((function(){$("#menu-anchors").on("click","a",(function(t){t.preventDefault();var n=$(this).attr("href"),o=$(n).offset().top;$("body,html").animate({scrollTop:o},1500)}))}));
+//# sourceMappingURL=index.28bcbb4f.js.map
